425fc2a6e734ffbd983baad2f114cd7e7513afe8
nkrisfalusy <nkrisfalusy@gmail.com>
2016-05-05 12:40:32 -0400

    Update include.php

--- include.php
+++ include.php
@@ -1,22 +1,6 @@
 <?php
 	$sqlhost = "localhost";
-<<<<<<< HEAD
-<<<<<<< HEAD
-	/*$sqluser = "kydbmaster";
-	$sqlpass = "]??}ZDRH]n(E";
-	$sqldb = "owgf";*/
-	
-	$sqluser = "root";
-	$sqlpass = "";
-	$sqldb = "wdw";
-=======
 	$sqluser = "kydbmaster";
 	$sqlpass = "]??}ZDRH]n(E";
 	$sqldb = "owgf";
->>>>>>> origin/master
-=======
-	$sqluser = "kydbmaster";
-	$sqlpass = "]??}ZDRH]n(E";
-	$sqldb = "owgf";
->>>>>>> origin/master
 ?>
